809ee4855b9255ccbb3ec30c8d34d2fde55021dd
Alejandro Ruiz <kannder.ziur@gmail.com>
2022-05-24 20:27:28 -0500

    fix styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,21 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
-
-  const search =(event)=>{
-    event.preventDefault()
-    console.log("Searching...")
-  }
+  const search = (event) => {
+    event.preventDefault();
+    // console.log("Searching...")
+  };
 
   return (
-    <section className='w-full h-auto flex flex-col justify-center items-center bg-gradient-to-t from-morado/60 to-azul/60'>
-      <form onSubmit={search} className='w-full mt-64 mb-64 md:max-w-5xl mx-auto p-4 flex flex-col justify-center items-center'>
-        <label className='text-white text-5xl mb-4'>Find your new job!</label>
-        <input type="email" className='w-full my-2 p-2 rounded-full outline-none focus:bg-red-10 hover:shadow-lg focus:ring-2'/>
-        <button className='btn-primary my-2'>Find Job</button>
+    <section className="w-full h-screen flex flex-col justify-center items-center bg-gradient-to-t from-morado/60 to-azul/60">
+      <form
+        onSubmit={search}
+        className="w-full mt-64 mb-64 md:max-w-5xl mx-auto p-4 flex flex-col justify-center items-center"
+      >
+        <label className="text-white text-5xl mb-4">Find your new job!</label>
+        <input
+          type="email"
+          className="w-full my-2 p-2 rounded-full outline-none focus:bg-red-10 hover:shadow-lg focus:ring-2"
+        />
+        <button className="btn-primary my-2">Find Job</button>
       </form>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,16 +1,41 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { MainTitle } from "../components/MainTitle";
 
+import { Input } from "../components/Input";
+
 const User = () => {
   const { auth, setAuth } = useContext(AuthContext);
+  const name = useRef();
+  const email = useRef();
+
+  useEffect(() => {
+    name.current.value = auth.name;
+    email.current.value = auth.email;
+  }, []);
 
   return (
     <div className="w-full bg-lila/90">
       <MainTitle title="User" />
-      <section className="w-full py-20 flex justify-center items-center">
+      <form className="w-96 mx-auto h-screen py-20 flex flex-col justify-center items-center">
+        <h3>ID: {auth.id}</h3>
+        <Input
+          inputTitle="Name"
+          inputType="text"
+          inputRef={name}
+          inputPlaceholder=""
+        />
+        <Input
+          inputTitle="Email"
+          inputType="email"
+          inputRef={email}
+          inputPlaceholder=""
+        />
         <pre>{JSON.stringify(auth, null, 2)}</pre>
-      </section>
+        <button className="px-4 py-2 rounded-md bg-rosa/90 hover:bg-rosa hover:shadow-md hover:text-white">
+          Update
+        </button>
+      </form>
     </div>
   );
 };
